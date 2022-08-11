d3bb241ed0b0f8bc790de1b7f4c794dabe21ba6b
Igor Koneshov <igorkoneshov@mail.ru>
2022-08-11 22:47:42 +0400

    feat: add logic to select chat

--- src/component/ui/CompactChat.tsx
+++ src/component/ui/CompactChat.tsx
@@ -1,24 +1,37 @@
 import React, { FC } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hook/rtkhook';
+import { toogleSelectChat } from '../../store/chatSlice';
 import { CompactChatProps } from '../../types/Chat';
 
 const CompactChat: FC<CompactChatProps> = ({
-	avatar,
-	name,
-	time,
-	lastMessage,
+  avatar,
+  id,
+  name,
+  time,
+  lastMessage,
 }) => {
-	return (
-		<div className='compact-chat'>
-			<img src={avatar} alt={name} className='compact-chat__img' />
-			<div className='compact-chat__container'>
-				<div className='compact-chat__info'>
-					<h3 className='compact-chat__name'>{name}</h3>
-					<p className='compact-chat__time'>{time}</p>
-				</div>
-				<p className='compact-chat__last-message'>{lastMessage}</p>
-			</div>
-		</div>
-	);
+  const dispatch = useAppDispatch();
+  const { isSelect, userId } = useAppSelector(state => state.chat);
+  const selectChat = () => {
+    dispatch(toogleSelectChat({ userName: name, userId: id }));
+  };
+  return (
+    <div
+      className={`compact-chat ${
+        isSelect && userId === id && 'compact-chat_active'
+      }`}
+      onClick={selectChat}
+    >
+      <img src={avatar} alt={name} className='compact-chat__img' />
+      <div className='compact-chat__container'>
+        <div className='compact-chat__info'>
+          <h3 className='compact-chat__name'>{name}</h3>
+          <p className='compact-chat__time'>{time}</p>
+        </div>
+        <p className='compact-chat__last-message'>{lastMessage}</p>
+      </div>
+    </div>
+  );
 };
 
 export default CompactChat;
--- src/store/chatSlice.ts
+++ src/store/chatSlice.ts
@@ -1,37 +1,34 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface ChatState {
-	isSelect: boolean;
-	userName: string;
-	userId: number;
+  isSelect: boolean;
+  userName: string;
+  userId: number;
 }
 
 interface ChatData {
-	userId: number;
-	userName: string;
+  userId: number;
+  userName: string;
 }
 
 const initialState: ChatState = {
-	isSelect: false,
-	userId: -1,
-	userName: '',
+  isSelect: false,
+  userId: -1,
+  userName: '',
 };
 
 const chatSlice = createSlice({
-	name: 'chat',
-	initialState: initialState,
-	reducers: {
-		setChatData(state, action: PayloadAction<ChatData>) {
-			state.userId = action.payload.userId;
-			state.userName = action.payload.userName;
-			state.isSelect = true;
-		},
-		toogleSelectChat(state, action: PayloadAction<boolean>) {
-			state.isSelect = action.payload;
-		},
-	},
+  name: 'chat',
+  initialState: initialState,
+  reducers: {
+    toogleSelectChat(state, action: PayloadAction<ChatData>) {
+      state.userId = action.payload.userId;
+      state.userName = action.payload.userName;
+      state.isSelect = true;
+    },
+  },
 });
 
-export const {} = chatSlice.actions;
+export const { toogleSelectChat } = chatSlice.actions;
 
 export default chatSlice.reducer;
