6c1f08fae8ff6bc98da5aa5b302566ab8725c64e
Igor Koneshov <igorkoneshov@mail.ru>
2022-08-12 01:17:18 +0400

    feat: create ChatError

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import ChatError from '../component/ui/ChatError';
 import ChatList from '../component/ui/ChatList';
 import Messenger from '../component/ui/Messenger';
 import SideBar from '../component/ui/SideBar';
+import { useAppSelector } from '../hook/rtkhook';
 import '../styles/Main.scss';
 
 const Main: React.FC = () => {
+  const isSelect = useAppSelector(state => state.chat.isSelect);
   return (
     <div className='main'>
       <SideBar />
       <ChatList />
-      <Messenger />
+      {isSelect ? <Messenger /> : <ChatError />}
     </div>
   );
 };
